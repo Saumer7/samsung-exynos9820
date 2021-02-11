691c3a4943d5224a2e5d0df7cc63da2b6292edf2
Saumer7 <31887385+Saumer7@users.noreply.github.com>
2021-02-12 01:49:28 +0900

    Update gpu_custom_interface.c
    2,4~8,10. -110 2575-2465

--- drivers/gpu/arm/b_r19p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/b_r19p0/platform/exynos/gpu_custom_interface.c
@@ -2462,6 +2462,7 @@ void gpu_remove_sysfs_file(struct device *dev)
 	device_remove_file(dev, &dev_attr_vol);
 	device_remove_file(dev, &dev_attr_power_state);
 	device_remove_file(dev, &dev_attr_asv_table);
+	device_remove_file(dev, &dev_attr_volt_table);
 	device_remove_file(dev, &dev_attr_dvfs_table);
 	device_remove_file(dev, &dev_attr_time_in_state);
 	device_remove_file(dev, &dev_attr_utilization);
